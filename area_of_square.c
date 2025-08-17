2b124cee545a9fe3dbe2504f3e248d812ff40f16
Sejal-2004 <sejalsingh8647@gmail.com>
2025-08-17 22:22:36 +0530

    Area of Square

--- area_of_square.c
+++ area_of_square.c
@@ -3,6 +3,6 @@ int main(){
     int side;
     printf("Enter the side of square:");
     scanf("%d",&side);
-    printf("The area of square with given side (%d) is %d.",side,side*side);
+    printf("The area of square with given side is %d.",side*side);
     return 0;
 }
